97c12422dc88b65343e24b80f90828eab8f88621
Saif Shovon <saaifshovon@gmail.com>
2018-06-02 14:54:46 +0600

    Clock App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-ReactDOM.render(
 
-<h1>Welcome My First React App by Saif Shovon</h1>,
+setInterval(function(){
+ReactDOM.render(
+	<h2> Time: {new Date().toLocaleTimeString()}</h2>,
 document.getElementById('root')
 	);
+
+},1000);
